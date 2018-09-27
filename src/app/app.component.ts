435759d6102e0543bd8e767d695f45092cbb0c5c
Robert Roach <rjayroach@gmail.com>
2018-09-27 10:19:41 +0000

    google maps (agm) first pass

---
+++ src/app/app.component.ts
@@ -0,0 +1,137 @@
+import { Component } from '@angular/core';
+// import { DataService } from './data.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+  // providers: [DataService]
+})
+export class AppComponent implements OnInit {
+  title: string = 'maps'
+  lat: number = 41.29364
+  lng: number = 103.83223
+  lable: string = 'AB'
+  locationChosen: boolean = false
+  markers: marker[] =
+  [
+    {
+      lat: 1.2936014,
+      lng: 103.832211,
+      label: 'start',
+      color: 'green'
+    },
+    {
+      lat: 1.2919227645330837,
+      lng: 103.81586025384524,
+      label: 't',
+      color: 'blue'
+    },
+    {
+      lat: 1.3090633620231047,
+      lng: 103.78866232058863,
+      label: 'end',
+      color: 'red'
+    }
+  ]
+
+  // constructor(private dataService: DataService) { }
+
+  ngOnInit() {
+    this.getUserLocation()
+    // this.dataService.fetchData().subscribe(
+    //   (data) => this.markers = data
+    // )
+  }
+
+  labelOptions = {
+    // color: '#CC0000',
+    color: 'green',
+    // fontFamily: '',
+    fontSize: '14px',
+    //fontWeight: 'bold',
+    text: 'Start',
+  }
+
+  trackMe() {
+    if (navigator.geolocation) {
+      this.isTracking = true;
+      alert("Geolocation tracking started.");
+      navigator.geolocation.watchPosition((position) => {
+        this.showTrackingPosition(position);
+      });
+    } else {
+      alert("Geolocation is not supported by this browser.");
+    }
+  }
+
+  showTrackingPosition(position) {
+    this.lat = position.coords.latitude;
+    this.lng = position.coords.longitude;
+    console.log(this.lat)
+    console.log(this.lng)
+
+
+    // let location = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+    // this.map.panTo(location);
+
+    // if (!this.marker) {
+    //   this.marker = new google.maps.Marker({
+    //     position: location,
+    //     map: this.map,
+    //     title: 'Got you!'
+    //   });
+    // }
+    // else {
+    //   this.marker.setPosition(location);
+    // }
+  }
+
+  addCheck(checkType) {
+    console.log('hello!')
+    // store the current location in firebase and add type 't-check'
+    // check that ninja guy's video for updating belt color on add to firebase
+    // add a marker to the map at current location
+    this.getUserLocation()
+    console.log(this.lat)
+    console.log(this.lng)
+    console.log(checkType)
+  }
+
+  getUserLocation() {
+    if (navigator.geolocation) {
+      console.log('Geolocation is supported')
+      navigator.geolocation.getCurrentPosition(position => {
+        console.log(position.coords)
+        this.lat = position.coords.latitude
+        this.lng = position.coords.longitude
+      }, (error) => {
+       switch (error.code) {
+           case 1:
+               alert('Permission Denied');
+               break;
+           case 2:
+               alert('Position Unavailable');
+               break;
+           case 3:
+               alert('Timeout');
+               break;
+       }}
+    } else {
+      console.log('Geolocation is not supported for this Browser/OS.');
+    }
+  }
+  onChoseLocation(event) {
+    this.lat = event.coords.lat
+    this.lng = event.coords.lng
+    this.locationChosen = true
+    console.log(event)
+  }
+
+}
+
+interface marker {
+  lat: number
+  lng: number
+  label: string
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, ApplicationRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { AgmCoreModule } from '@agm/core';
+// import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    // HttpClientModule,
+    CommonModule,
+    FormsModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyDzHfZcz7SxIdFy_AZyl1RusntZgPwla2Q'
+    })
+  ],
+  providers: [],
+  bootstrap: [AppComponent] //, HTTP_PROVIDERS]
+})
+export class AppModule { }
